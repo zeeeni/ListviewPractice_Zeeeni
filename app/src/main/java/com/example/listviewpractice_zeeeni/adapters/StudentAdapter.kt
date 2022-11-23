8bd13affefc77cd632b579c3b83d9d48c32cf0b9
Jieun <wldms941114@gmail.com>
2022-11-23 21:53:19 +0900

    Adapter 뼈대 작업 마무리

--- app/src/main/java/com/example/listviewpractice_zeeeni/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_zeeeni/adapters/StudentAdapter.kt
@@ -1,11 +1,32 @@
 package com.example.listviewpractice_zeeeni.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listviewpractice_zeeeni.R
 import com.example.listviewpractice_zeeeni.datas.Student
 
 class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+        val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+
+    }
+
 }
