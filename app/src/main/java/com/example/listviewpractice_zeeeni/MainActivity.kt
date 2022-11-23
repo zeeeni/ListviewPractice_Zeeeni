51c03805caf7c996293198c9e864065fcd8ac045
Jieun <wldms941114@gmail.com>
2022-11-23 22:14:01 +0900

    학생목록 ArrayList 생성, 실제 목록 추가

--- app/src/main/java/com/example/listviewpractice_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_zeeeni/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.listviewpractice_zeeeni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice_zeeeni.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( Student("김지은", 1994))
+        mStudentList.add( Student("김정은", 1997))
+        mStudentList.add( Student("김예은", 2000))
+        mStudentList.add( Student("김도환", 2006))
+        mStudentList.add( Student("김호섭", 1966))
+        mStudentList.add( Student("김춘화", 1972))
+        mStudentList.add( Student("전한솔", 1994))
+
     }
 }
