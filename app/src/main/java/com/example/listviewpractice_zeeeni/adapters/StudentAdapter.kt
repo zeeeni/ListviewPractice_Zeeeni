f4d2cd90511c63d342bbecb4ceb0582a6f1e576a
Jieun <wldms941114@gmail.com>
2022-11-23 23:02:29 +0900

    출생년도 -> 한국식 나이(2021)로 계산해서 출력

--- app/src/main/java/com/example/listviewpractice_zeeeni/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_zeeeni/adapters/StudentAdapter.kt
@@ -33,7 +33,11 @@ class StudentAdapter(
 
         nameTxt.text = studentData.name
 
-        birthYearTxt.text = "(${studentData.birthYear}년생)"
+//        출생년도를 가지고 => (2022년 한국식) 나이로 변환해서 보여주기
+
+        val koreanAge = 2022 - studentData.birthYear +1
+
+        birthYearTxt.text = "(${koreanAge}세)"
 
         return row
 
